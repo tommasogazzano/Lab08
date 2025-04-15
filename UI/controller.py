0c8e50f8f5e4bdc6d1190c210a1b1c610c3266dc
tommasogazzano <tommaso.gazzano@gmail.com>
2025-04-15 15:32:07 +0200

    finish

--- UI/controller.py
+++ UI/controller.py
@@ -5,9 +5,7 @@ from model.nerc import Nerc
 
 class Controller:
     def __init__(self, view, model):
-        # the view, with the graphical elements of the UI
         self._view = view
-        # the model, which implements the logic of the program and holds the data
         self._model = model
         self._idMap = {}
         self.fillIDMap()
@@ -28,7 +26,6 @@ class Controller:
             total_affected = sum(event.customers_affected for event in sol)
             total_hours = sum(self._model.calcolaOre(event) for event in sol)
 
-            # Aggiungi le informazioni di riepilogo
             self._view._txtOut.controls.append(
                 ft.Text(f"Tot people affected: {total_affected}")
             )
@@ -36,21 +33,10 @@ class Controller:
                 ft.Text(f"Tot hours of outage: {total_hours:.2f}")
             )
 
-            # Aggiungi una riga vuota per separare
             self._view._txtOut.controls.append(ft.Text(""))
 
-            # Aggiungi ogni evento alla ListView come controllo Text
             for event in sol:
-                # Formatta le date come nell'esempio
-                start_time = event.date_event_began
-                end_time = event.date_event_finished
-
-                # Crea una rappresentazione leggibile dell'evento
-                event_info = (
-                    f"id={event.id}, customers_affected={event.customers_affected} "
-                    f"start_time={start_time}, end_time= {end_time}"
-                )
-                self._view._txtOut.controls.append(ft.Text(event_info))
+                self._view._txtOut.controls.append(ft.Text(event.__str__()))
         self._view.update_page()
 
     def fillDD(self):
